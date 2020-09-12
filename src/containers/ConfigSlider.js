a6c48505912800a21add75192d6479db6bd78ceb
Aleksei Shmelev <ashmelev@kr.digital>
2020-09-12 19:24:53 +0300

    Configurator reworked

---
+++ src/containers/ConfigSlider.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Link, Popover, Slider } from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const ConfigSlider = (props) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleLinkClicked = (event) => {
+    setAnchorEl(event.target)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <>
+      <Link onClick={handleLinkClicked}>
+        {props.value}
+      </Link>
+      <Popover 
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+      >
+      <div style={{
+        minHeight: '40px',
+        minWidth: '300px',
+        padding: '0 30px'
+      }}>
+        <Slider 
+          step={1}
+          marks
+          min={1}
+          value={props.value}
+          onChange={(_, val) => props.action(val)}
+          max={props.maxValue || 15}
+        />
+      </div>
+      </Popover>
+    </>
+  );
+}
+
+ConfigSlider.propTypes = {
+  value: PropTypes.string.isRequired,
+  maxValue: PropTypes.number,
+  action: PropTypes.func.isRequired
+}
+
+export default ConfigSlider;
--- src/containers/ModeConfigurator.js
+++ src/containers/ModeConfigurator.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import * as modeTypes from '../utility/modeTypes';
-import ConfigSlider from '../components/ConfigSlider';
 import PropTypes from 'prop-types';
 import Loader from './Loader';
-import { Button, Box, MenuItem, FormControl, InputLabel, Select, Container, TextField } from '@material-ui/core';
+import { Button, Box, MenuItem, FormControl, InputLabel, Select, Container, TextField, Typography, Stepper, Step, StepLabel, StepContent, Link } from '@material-ui/core';
+import ConfigSlider from './ConfigSlider';
 
 const ModeConfigurator = (props) => {
   const { showError } = props;
@@ -51,44 +51,98 @@ const ModeConfigurator = (props) => {
     )
   }
 
+  const steps = {
+    SELECT_PLAYLIST: 'Select playlist',
+    SET_TRACKS_THRESHOLD: 'Set tracks threshold',
+    SET_RELATED_ARTISTS_LIMIT: 'Set related artists limit',
+    SET_TRACKS_LIMIT_PER_ARTIST: 'Set tracks limit per artist',
+    SELECT_PLAYLIST_NAME: 'Select playlist name'
+  }
+
+  const getStepContent = (step) => {
+    switch (step) {
+      case steps.SELECT_PLAYLIST:
+        let playlistItems = null;
+        if (playlists) {
+          playlistItems = playlists.map(p => <MenuItem key={p.id} value={p.id} >{p.name} ({p.tracksTotal})</MenuItem>)
+        }
+        
+        const playlistsSelector = (
+          <FormControl style={{minWidth: 300}}>
+            <Select labelId="playlistsSelect" value={modeConfig.selectedPlaylist} onChange={(event) => configValueChangedHandler('selectedPlaylist', event.target.value)}>
+              <MenuItem value={0}>Library</MenuItem>
+              { playlistItems }
+            </Select>
+          </FormControl>
+        )
+
+        return <Typography>
+          Firstly, it will collect all of your saved tracks from {playlistsSelector}
+        </Typography>;
+
+      case steps.SET_TRACKS_THRESHOLD:
+        const artistThresholdParam = <ConfigSlider 
+          action={(value) => configValueChangedHandler('artistTracksThreshold', value)}
+          value={modeConfig.artistTracksThreshold}
+          maxValue={15}
+        />
+
+        return <Typography>
+          Then it will group those tracks by artists. And filter out artists with more than {artistThresholdParam} tracks saved.
+        </Typography>;
+
+      case steps.SET_RELATED_ARTISTS_LIMIT:
+        const relatedArtistsParam = <ConfigSlider 
+          action={(value) => configValueChangedHandler('relatedArtistsQuantity', value)}
+          value={modeConfig.relatedArtistsQuantity}
+          maxValue={20}
+        />
+
+        return <Typography>
+          For each of the selected artist it will get {relatedArtistsParam} related artists, but only get the ones who has no saved tracks in your library.
+        </Typography>;
+
+      case steps.SET_TRACKS_LIMIT_PER_ARTIST:
+        const targetQuantityParam = <ConfigSlider 
+          title="Target tracks quantity per artist"
+          action={(value) => configValueChangedHandler('targetQuantityPerArtist', value)}
+          value={modeConfig.targetQuantityPerArtist}
+          maxValue={10}
+        />
+
+        return <Typography>
+          For each entry of the final artists list it will get {targetQuantityParam} of it's top tracks.
+        </Typography>;
+
+      case steps.SELECT_PLAYLIST_NAME:
+        const playlistNameParam = <TextField 
+          value={modeConfig.newPlaylistName}
+          onChange={(event) => configValueChangedHandler("newPlaylistName", event.target.value)}
+        />
+        return <Typography>
+          Finally, it will add the whole bunch of selected tracks (about 1245 at max) to the newely created playlist {playlistNameParam}
+        </Typography>;
+
+      default:
+        return null;
+    }
+  }
+
   return (
     <>    
       <Container>
         <Box>
-          <TextField 
-            label="New playlist name" 
-            value={modeConfig.newPlaylistName}
-            onChange={(event) => configValueChangedHandler("newPlaylistName", event.target.value)}
-          />
-
-          <ConfigSlider 
-            title="Minimal tracks threshold for one artist"
-            action={(value) => configValueChangedHandler('artistTracksThreshold', value)}
-            value={modeConfig.artistTracksThreshold}
-            maxValue={15}
-          />
-
-          <ConfigSlider 
-            title="Target tracks quantity per artist"
-            action={(value) => configValueChangedHandler('targetQuantityPerArtist', value)}
-            value={modeConfig.targetQuantityPerArtist}
-            maxValue={10}
-          />
-
-          {
-            props.mode === modeTypes.DIVE_DEEPER && (
-              <ConfigSlider 
-                title="Target related artists quantity"
-                action={(value) => configValueChangedHandler('relatedArtistsQuantity', value)}
-                value={modeConfig.relatedArtistsQuantity}
-                maxValue={20}
-              />
-            )
-          }
-        </Box>
-          
-        <Box>
-          { playlistsSelector }
+          <Typography variant="h4">HOW IT WORKS</Typography>
+          <Stepper nonLinear={true} orientation="vertical">
+            {
+              Object.keys(steps).map(stepKey => (
+                <Step key={stepKey} active>
+                  <StepLabel>{steps[stepKey]}</StepLabel>
+                  <StepContent>{getStepContent(steps[stepKey])}</StepContent>
+                </Step>
+              ))
+            }
+          </Stepper>
         </Box>
       </Container>
 
